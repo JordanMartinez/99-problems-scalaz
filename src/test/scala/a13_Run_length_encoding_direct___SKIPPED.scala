8cd7de62ffb372c2b5727bfc0defc0c4caf50dde
Jordan Martinez <jordanalex.martinez@gmail.com>
2018-06-19 15:00:11 -0700

    Explain why I skipped number 13

--- src/test/scala/a13_Run_length_encoding_direct___SKIPPED.scala
+++ src/test/scala/a13_Run_length_encoding_direct___SKIPPED.scala
@@ -1,5 +1,6 @@
 import org.scalacheck.Properties
 
+// Skipped because I didn't understand how "direct method" would be different from what I was already doing...
 object a13_Run_length_encoding_direct___SKIPPED extends Properties("run-length encoding (direct) of list") {
 
 }
