9be6fa861a27fa2f3ad61d5c1f3801fee5c406a0
Jordan Martinez <jordanalex.martinez@gmail.com>
2018-06-27 09:43:33 -0700

    Solution to number 22

---
+++ src/test/scala/a22_Create_list_of_all_integers_within_range.scala
@@ -0,0 +1,23 @@
+import scala.Int
+import org.scalacheck._
+import scalaz._
+import Scalaz._
+
+import scala.annotation.tailrec
+
+object a22_Create_list_of_all_integers_within_range extends Properties("Create list of all integers within range") {
+
+  property("") = {
+    IList(4, 5, 6, 7, 8, 9) === range(4, 9)
+  }
+
+  def range(from: Int, to: Int): IList[Int] = {
+    @tailrec
+    def go(from: Int, to: Int, acc: IList[Int]): IList[Int] = {
+      if (from == to) from +: acc
+      else go(from, to - 1, to +: acc)
+    }
+    go(from, to, INil())
+  }
+
+}
