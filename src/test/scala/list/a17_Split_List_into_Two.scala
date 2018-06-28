d4fbb057daeb18c1b9e9b0ce9841b06985182c83
Jordan Martinez <jordanalex.martinez@gmail.com>
2018-06-28 14:16:33 -0700

    Problem 17: Use tail recursive function instead of foldLeft

--- src/test/scala/list/a17_Split_List_into_Two.scala
+++ src/test/scala/list/a17_Split_List_into_Two.scala
@@ -6,6 +6,8 @@ import org.scalacheck._
 import scalaz.Scalaz._
 import scalaz._
 
+import scala.annotation.tailrec
+
 object a17_Split_List_into_Two extends Properties("Split list into two") {
 
   property("") = {
@@ -15,10 +17,13 @@ object a17_Split_List_into_Two extends Properties("Split list into two") {
   }
 
   def split(n: Int, l: IList[Symbol]): (IList[Symbol], IList[Symbol]) = {
-    val (_, left, right) = l.foldLeft((n, IList.empty[Symbol], IList.empty[Symbol]))((acc, next) =>
-      if (acc._1 == 0) (acc._1, acc._2, acc._3 :+ next)
-      else (acc._1 - 1, acc._2 :+ next, acc._3)
-    )
-    (left, right)
+    @tailrec
+    def go[A](n: Int, right: IList[A], left: IList[A]): (IList[A], IList[A]) = {
+      if (n == 0) (left.reverse, right) else right match {
+        case INil() => (INil(), INil())
+        case ICons(h, tail) => go(n - 1, tail, h +: left)
+      }
+    }
+    go(n, l, INil())
   }
 }
