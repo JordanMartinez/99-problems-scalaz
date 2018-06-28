2fb0379f4a2653a4b598c9806a7e2e79900852f6
Jordan Martinez <jordanalex.martinez@gmail.com>
2018-06-27 18:14:38 -0700

    Solution to number 27 (full)

--- src/test/scala/list/a27_Group_elements_of_set_into_disjoint_subsets.scala
+++ src/test/scala/list/a27_Group_elements_of_set_into_disjoint_subsets.scala
@@ -1,6 +1,6 @@
 package list
 
-import scala.Int
+import scala.{Int, StringContext}
 
 import org.scalacheck.Prop._
 import org.scalacheck._
@@ -13,42 +13,49 @@ object a27_Group_elements_of_set_into_disjoint_subsets extends Properties(
   "Group elements of a set into disjoint subsets") {
 
   property("") = {
+    val sizeList = IList(1, 3, 2)
     val originalList = IList("Aldo", "Beat", "Carla", "David", "Evi", "Flip", "Gary", "Hugo", "Ida")
-    val result = group(2, 3, originalList)
+    val result = group(sizeList, originalList)
 
     // print results using hack
-    result.map(group => java.lang.System.out.println("One group: " + group))
+    result.map(group => java.lang.System.out.println(s"One group: $group"))
 
     result.length === originalList.length
-    result.all(group => group.length === 3)
+    val expectedLength = sizeList.length
+    result.all(group => group.length === expectedLength)
   }
 
-  def group[A](firstSize: Int, secondSize: Int, l: IList[A]): IList[IList[IList[A]]] = {
-    @tailrec
-    def getSecond[C](n: Int, acc: IList[C], l: IList[C]): IList[IList[C]] = {
-      l match {
-        case INil() => INil()
-        case ICons(h, tail) if n == 0 => IList((h +: acc).reverse, tail)
-        case ICons(h, tail) => getSecond[C](n - 1, h +: acc, tail)
-      }
-    }
+  def group[A](sizes: IList[Int], l: IList[A]): IList[IList[IList[A]]] = {
+    // Note: this code is only in case one happens to input in invalid "sizes" argument
+    // --------------------------------------------------------------------------------
+    val listSlots = l.length
+    val desiredSlots = sizes.suml
+    if (listSlots < desiredSlots)
+      scala.sys.error(s"Your request wants $desiredSlots total people but original list only has $listSlots (${desiredSlots - listSlots} additional people required)")
+    // --------------------------------------------------------------------------------
 
-    @tailrec
-    def getFirst[B](n: Int, second: Int, acc: IList[B], l: IList[B]): IList[IList[IList[B]]] = {
-      l match {
-        case INil() => INil()
-        case ICons(h, tail) if n == 0 => IList(IList((h +: acc).reverse) |+| getSecond[B](second, INil(), tail))
-        case ICons(h, tail) => getFirst[B](n - 1, second, h +: acc, tail)
+    def getGroup[B](sizes: IList[Int], acc: IList[B], l: IList[B]): IList[IList[B]] = {
+      (l, sizes) match {
+        case (INil(), _) => INil()
+
+        // final part found, return but without the empty tail
+        case (ICons(h, INil()), ICons(1, INil())) => IList((h +: acc).reverse)
+        // final part found, return
+        case (ICons(h, tail), ICons(1, INil())) => IList((h +: acc).reverse, tail)
+        // current part found and finished, reset loop to build next part
+        case (ICons(h, tail), ICons(1, sizeTail)) => IList((h +: acc).reverse) |+| getGroup(sizeTail, INil(), tail)
+        // recursive case: building accumulation for current part
+        case (ICons(h, tail), ICons(current, sizeTail)) => getGroup[B](ICons(current - 1, sizeTail), h +: acc, tail)
       }
     }
 
     // NOTE: NOT STACK SAFE | TODO: Make Stack-Safe
     def rotateAndConcat(n: Int, l: IList[A]): IList[IList[IList[A]]] = {
-      if (n == 0) getFirst(firstSize - 1, secondSize - 1, INil(), l)
-      else getFirst(firstSize - 1, secondSize - 1, INil(), l) |+| rotateAndConcat(n - 1, rotateLeft(1, l))
+      if (n == 0) IList(getGroup(sizes, INil(), l))
+      else rotateAndConcat(n - 1, rotateLeft(1, l)) |+| IList(getGroup(sizes, INil(), l))
     }
 
-    rotateAndConcat(l.length - 1, l).reverse
+    rotateAndConcat(l.length - 1, l)
   }
 
   def rotateLeft[A](n: Int, l: IList[A]): IList[A] = {
